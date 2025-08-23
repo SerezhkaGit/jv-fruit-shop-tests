804835958569c8925faf0c1723b277ec10d1c1bd
Serhiy <sergiy.kusiv@gmail.com>
2025-08-23 16:43:23 +0300

    Implemented tests for FileWritter

--- src/test/java/core/basesyntax/MainTests.java
+++ src/test/java/core/basesyntax/MainTests.java
@@ -1,14 +1,20 @@
 package core.basesyntax;
 
+import core.basesyntax.service.FileWriter;
 import core.basesyntax.service.impl.FileReaderImpl;
+import core.basesyntax.service.impl.FileWriterImpl;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTests {
     private final FileReaderImpl fileReader = new FileReaderImpl();
+    private final FileWriterImpl fileWriter = new FileWriterImpl();
 
     @Test
     void read_ShouldReadFileSuccessufully() {
@@ -28,4 +34,28 @@ public class MainTests {
         assertTrue(exception.getMessage().contains("Can't read file"));
     }
 
+    @Test
+    void write_ShouldWriteInfoToFileSuccessfully() throws IOException {
+
+        String report = "fruit,quantity\napple,10\nbanana,5";
+        Path tempFile = Files.createTempFile("finalReport", ".csv");
+
+        fileWriter.write(report, tempFile.toString());
+
+        String actual = Files.readString(tempFile);
+        assertEquals(report, actual);
+
+        Files.deleteIfExists(tempFile);
+    }
+
+    @Test
+    void write_ShouldThrowException_WhenFilePathInvalid() {
+        String report = "test data";
+        String invalidPath = "/invalid-path/finalReport.csv";
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> fileWriter.write(report, invalidPath));
+
+        assertTrue(exception.getMessage().contains("Can't write to file"));
+    }
 }
